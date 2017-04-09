6534227701cea143e752d1f21c70589198f5cb73
JuaniFilardo <jifilardo@gmail.com>
2017-04-09 13:26:48 -0300

    Added a few methods to make the output more useful. Updated the input file

--- src/LittleParser/Main.java
+++ src/LittleParser/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sim_tp2;
+package LittleParser;
 
 import java.util.Scanner;
 import java.io.File;
@@ -20,17 +20,23 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         File file = new File("datacount.txt");
-        
+
         ArrayList<String> al = new ArrayList<>();
         String prefix = "data-count=\"";
         String suffix = "\"";
-        
-        al = getValuesAfterString(file,prefix,suffix);
-        
+
+        al = getValuesAfterString(file, prefix, suffix);
+
         System.out.println(al.toString());
         System.out.println("Number of values: " + al.size());
+
+        int[] cont = countFrequencyOnIntervals(al, 10, getBiggest(al));
+
+        System.out.println("Interval frequency: ");
+        System.out.println(printArray(cont));
+
     }
 
     /**
@@ -49,11 +55,6 @@ public class Main {
      */
     private static ArrayList<String> getValuesAfterString(File file, String prefix, String suffix) {
 
-        /*
-        Acá estaría usando un prefix "data-count="" (sic)
-        y un suffix """ (sic) que de antemano sé que existen y que están en
-        la misma línea
-         */
         ArrayList<String> al = new ArrayList<>();
 
         try (Scanner sca = new Scanner(file)) {
@@ -81,7 +82,7 @@ public class Main {
 
                     // Look for the suffix
                     int suffixIndex = cutLine.indexOf(suffix);
-                    
+
                     // If there's a match
                     if (suffixIndex >= 0) {
 
@@ -110,6 +111,86 @@ public class Main {
         }
 
         return al;
+    }
+
+    /**
+     * If you find a better way, ¯\_(ツ)_/¯
+     * Counts how many numbers are there in
+     * each interval. The first interval goes from zero to (biggest/intervals),
+     * the second from (biggest/intervals) to (biggest/intervals)*2, and so on
+     * and so forth.
+     *
+     * @param al The ArrayList<String> which contains the numbers
+     * @param intervals The number of intervals we want
+     * @param biggest The biggest number on the ArrayList
+     * @return count An int array with the frequencies. count[0] has the
+     * frequency of the first interval, count[1] has the frequency of the second
+     * interval, and so on.
+     */
+    private static int[] countFrequencyOnIntervals(ArrayList<String> al, int intervals, int biggest) {
+
+        int[] count = new int[intervals];
+        // Set limit for the intervals
+        double fixedLimit = (double) biggest / intervals;
+
+        // For each number (actually, for each string)
+        for (String s : al) {
+
+            double varLimit = fixedLimit;
+            int foo = 1; // Fixed limit multiplier
+            int casillero = 0; // Interval to which the number belongs 
+
+            // While that's not the correct interval
+            while (Integer.parseInt(s) > varLimit) {
+                foo++;
+                varLimit = fixedLimit * foo;
+                casillero++;
+            }
+            // And when it finds its interval:
+            count[casillero]++;
+        }
+        return count;
+    }
+
+    /**
+     *
+     * @param num The array to be printed
+     * @return A String with format { i, j, k, l... }
+     */
+    private static String printArray(int[] num) {
+
+        String s = "{ ";
+
+        for (int i = 0; i < num.length; i++) {
+            s += String.valueOf(num[i]) + ", ";
+        }
+        return s.substring(0, s.length() - 2) + " }";
+    }
+
+    /**
+     *
+     * @param al The ArrayList to get the numbers from
+     * @return The biggest number on the ArrayList
+     */
+    private static int getBiggest(ArrayList<String> al) {
+
+        int i = 0;
+        int biggest = 0;
+
+        try {
+
+            for (String s : al) {
+                i = Integer.parseInt(s);
+
+                if (i > biggest) {
+                    biggest = i;
+                }
+
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+        return biggest;
     }
 
 }
